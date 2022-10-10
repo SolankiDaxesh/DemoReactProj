da9a8a74e9d77223f2c22368c4a960a2dc2b7db1
Daxesh Solanki <87466510+SolankiDaxesh@users.noreply.github.com>
2022-10-10 12:55:28 +0000

    ToUpper and ToLower completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,10 @@ function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About us" />
+      <hr />
       <div className='container my-3'>
         <TextForm heading="Enter the Text to analyze" />
       </div>
-
-
     </>
   )
 }
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -2,10 +2,16 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 export default function TextForm(props) {
+
   const handleUpClick = () => {
     console.log("Upper Case button clicked" + " " + text);
-    let newText = text.UpperCase;
-    setText(newText);
+    let newText = text.toUpperCase();
+    setText(newText)
+  }
+  const handleLoClick = () => {
+    console.log("Upper Case button clicked" + " " + text);
+    let newText = text.toLowerCase();
+    setText(newText)
   }
   const handleOnChange = (event) => {
     console.log("Handle on change")
@@ -14,12 +20,27 @@ export default function TextForm(props) {
   const [text, setText] = useState('Enter Some Text Here');
 
   return (
-    <div>
-      <h3>{props.heading} </h3>
-      <div className="mb-3">
-        <textarea className="form-control" id="myTextarea" value={text} onChange={handleOnChange} rows="8"></textarea>
+    <>
+      <div>
+        <h3>{props.heading} </h3>
+        <div className="mb-3" >
+          <textarea className="form-control  " id="myTextarea" placeholder='Enter Some Text Here' onChange={handleOnChange} rows="8"></textarea>
+        </div>
+        <button className='btn btn-outline-primary' onClick={handleUpClick}> Upper Case </button>
+        <button className='btn btn-outline-secondary mx-3' onClick={handleLoClick}> Lower Case </button>
       </div>
-      <button className='btn btn-primary' onClick={handleUpClick}>Convert to UpperCase</button>
-    </div>
+      <hr />
+      <div className='container my-3'>
+        <h3>Your Text Summery</h3>
+        <p><strong>{text.split(" ").length}</strong> words  and <strong>{text.length}</strong> characters</p>
+        <p><strong>{0.008 * text.split(" ").length}</strong> minutes will be taken to read </p>
+      </div>
+      <hr />
+      <div className="mb-3 ">
+        <h4>Output</h4>
+        <textarea class="form-control" type="text" value={text} placeholder='Enter Some Text Here' disabled readonly />
+        <hr />
+      </div>
+    </>
   )
 }
